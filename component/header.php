4a688648c2094d68f0bff9f44cc3315b3a40fbd4
royyy-han <mroyhan1808@gmail.com>
2026-05-20 13:54:12 +0700

    pullsai

--- component/header.php
+++ component/header.php
@@ -2,6 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tanah Blambangan</title>
-    <link rel="stylesheet" href="/tanah_blambangan/assetsWeb/css/wisata.css">
+    <link rel="stylesheet" href="../assetsWeb/css/wisata.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
